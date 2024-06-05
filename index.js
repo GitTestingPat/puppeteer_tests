c9133b0ef7625f53883871b6bf6980ad412438d8
Patricio Andrade Gallardo <patricioandradeh@gmail.com>
2024-06-05 17:29:28 -0400

    Changed comment in line 14

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const path = require('path');
     fs.mkdirSync(screenshotsFolder);
   }
 
-  // Establecer un viewport más grande para ver más elementos
+  // Set a larger viewport to view more elements
   await page.setViewport({ width: 1920, height: 1080 });
 
   await page.goto('https://englishexpressonline.com/english-express-online/');
